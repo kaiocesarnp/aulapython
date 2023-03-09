5f93dec20fa71a2a6c309b92393e4da633655405
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-09 19:12:58 -0300

    aprendendo py

--- aula02.py
+++ aula02.py
@@ -1,5 +1,16 @@
-resposta = int(input("Qual a resposta para tudo?\nR.: "))
+# resposta = int(input("Qual a resposta para tudo?\nR.: "))
 
-if resposta != 42:
-    print("Errou, tente novamente!")
+# if resposta != 42:
+#     print("Errou, tente novamente!")
 
+#---------------------------
+
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Pode dirigir no Brasil...")
+if idade < 18:
+    print("Não pode dirigir no Brasil...")
+if idade > 15:
+    print("Pode dirigir nos EUA...")
+if idade >= 16 and idade < 21:
+    print("Pode dirigir, mas não comprar álcool nos EUA...")
