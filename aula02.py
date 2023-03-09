57817a10a335e885b33dd39afa88ee6b07e80999
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-09 19:25:54 -0300

    aprendendo py

--- aula02.py
+++ aula02.py
@@ -5,12 +5,23 @@
 
 #---------------------------
 
+# idade = int(input("Digite sua idade: "))
+# if idade >= 18:
+#     print("Pode dirigir no Brasil...")
+# if idade < 18:
+#     print("Não pode dirigir no Brasil...")
+# if idade > 15:
+#     print("Pode dirigir nos EUA...")
+# if idade >= 16 and idade < 21:
+#     print("Pode dirigir, mas não comprar álcool nos EUA...")
+
+#---------------------------
+
 idade = int(input("Digite sua idade: "))
-if idade >= 18:
-    print("Pode dirigir no Brasil...")
-if idade < 18:
-    print("Não pode dirigir no Brasil...")
-if idade > 15:
-    print("Pode dirigir nos EUA...")
-if idade >= 16 and idade < 21:
-    print("Pode dirigir, mas não comprar álcool nos EUA...")
+if (idade > 16 or idade >= 70):
+    print("Pode votar!")
+    print("Já fez seu título de eleitor?")
+if (idade >= 18 and idade < 70):
+    print("Na sua idade, o voto é obrigatório.")
+if (idade < 16):
+    print("Você ainda não pode votar!")
