5527c8ad2c68fd9634de1fc18bbb465b66d14ce8
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-11 18:42:53 -0300

    aprendendo py

--- aula03.py
+++ aula03.py
@@ -2,7 +2,7 @@
 #OS itens vão se manter na ordem que você definiu;
 # Exceto se vc explicitamente pedir para que a ordem se alterada
 # Como listas possuem multiplos itens, utilize um nome no plural
-# 
+# O primeiro elemento de uma lista é o 0 (zero)
 
 lista_compras = ["maçãs", "leite", "arroz", "frango"]
 
