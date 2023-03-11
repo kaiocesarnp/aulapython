71d91169fd504f0a27c3b599b21a8920347597de
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-11 18:42:07 -0300

    aprendendo py

--- aula03.py
+++ aula03.py
@@ -3,3 +3,10 @@
 # Exceto se vc explicitamente pedir para que a ordem se alterada
 # Como listas possuem multiplos itens, utilize um nome no plural
 # 
+
+lista_compras = ["maçãs", "leite", "arroz", "frango"]
+
+for item in lista_compras:
+    print("Comprar:", item)
+
+    
