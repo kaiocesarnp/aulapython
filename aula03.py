74dd57237694e8ba2c96e7e7c6b653d80205ea35
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-11 18:55:32 -0300

    aprendendo py

--- aula03.py
+++ aula03.py
@@ -7,22 +7,26 @@
 
 lista_compras = ["maçãs", "leite", "arroz", "frango", "banana"]
 
-for item in lista_compras:
-    print("Comprar:", item)
+# for item in lista_compras:
+#     print("Comprar:", item)
 
 # O primeiro elemento de uma lista é o 0 (zero)
-print("O primeiro elemento é:", lista_compras[0])
-print("O primeiro terceiro é:", lista_compras[2])
+# print("O primeiro elemento é:", lista_compras[0])
+# print("O primeiro terceiro é:", lista_compras[2])
 
 # A função 'len' mostra o tamanho da lista
-tamanho = len(lista_compras)
-print("Tamanho:", tamanho)
+# tamanho = len(lista_compras)
+# print("Tamanho:", tamanho)
 
 # Pode-se usar o tamanho 'len' da lista para acessar o ultimo elemento da lista, por exemplo
-print("Ultimo:", lista_compras[len(lista_compras)-1])
+# print("Ultimo:", lista_compras[len(lista_compras)-1])
 
-for i in range(len(lista_compras)):
-    print("O elemento", i, "da lista é", lista_compras[i])
+# for i in range(len(lista_compras)):
+#     print("O elemento", i, "da lista é", lista_compras[i])
 
-
+# i = 0
+# while i < len(lista_compras):
+#     print("O elemento", i, "da lista é", lista_compras[i])
+#     i = i + 1
     
+# Pegando uma parte da lista/intervalo
