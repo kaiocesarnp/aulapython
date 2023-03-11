9d55b1b0ff329580fd718a4d60881ec4d848d275
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-11 18:39:37 -0300

    aprendendo py

---
+++ aula03.py
@@ -0,0 +1,5 @@
+# 01. Listas
+#OS itens vão se manter na ordem que você definiu;
+# Exceto se vc explicitamente pedir para que a ordem se alterada
+# Como listas possuem multiplos itens, utilize um nome no plural
+# 
