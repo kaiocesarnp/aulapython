605a9e2b79cdfa466e45574db3718ad148774b19
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-14 19:49:13 -0300

    py

--- aula04.py
+++ aula04.py
@@ -39,3 +39,40 @@
 
 # --------------------------------------------------------------
 
+# IMPRESSÃO DE DICIONÁRIOS E MÉTODOS
+
+# dic1 = {'banheiro': 3, 'sala': 2, 'quarto': 4}
+
+# print(dic1)
+# print(len(dic1)) #ver o tamanho do dicionario
+
+# ----------------------
+# imprimindo os elementos individualmente:
+# for chave in dic1:
+#     print(chave)
+
+# ------------------------
+# imprimindo os valores
+# for chave in dic1:
+#     print(dic1[chave])
+
+# -----------------------
+# imprimindo tudo em linhas separadas
+# for chave in dic1:
+#     print(chave, dic1[chave])
+
+# ------------------------
+# Pegando a lista de todas as chaves que tem no dicionário
+# dic2 = {'guarda-roupa': 3, 'televisão': 2, 'cadeira': 4, 'mesa': 1}
+# dic2.keys()
+
+# ----------------------
+# Pegando os valores da lista
+# dic = {'guarda-roupa': 3, 'televisão': 2, 'cadeira': 4, 'mesa': 1}
+# dic.values()
+
+# -----------------------
+# Associar o indice com o valor associado à chave que serve de indice
+# dic = {'guarda-roupa': 3, 'televisão': 2, 'cadeira': 4, 'mesa': 1}
+# dic.items()
+
