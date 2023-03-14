49966833bb3e3f2c0d7b50c22b1a09b2070f4e7f
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-14 16:47:43 -0300

    py

--- aula03.py
+++ aula03.py
@@ -323,3 +323,81 @@
 
 # ----------------------
 
+# ATIVIDADE
+
+# QUESTÃO 01
+# Considere a função a seguir, e indique o que a função faz.
+    # Assuma que o parâmetro lista sempre vai ser uma lista com 2 ou mais elementos.
+
+# def funcao(lista):
+#        fim = len(lista)-1
+#        inicio = 0
+#        while(inicio < fim):
+#                aux = lista[inicio]
+#                lista[inicio] = lista[fim]
+#                lista[fim] = aux
+#                inicio = inicio + 1
+#                fim = fim - 1
+
+# ---------------------------
+
+# QUESTÃO 02
+# Considere o trecho a seguir com uma função e sua chamada. Indique o que será impresso na tela.
+
+# def minha_funcao(valor):
+#         if(valor == 1):
+#                 return valor + 1;
+#         valor = valor + 10
+#         return valor + 20
+
+# a = 1
+# resultado = minha_funcao(a)
+# print(resultado)
+
+# ----------------------------
+
+# QUESTÃO 03
+# Considere o seguinte trecho de um programa em Python e indique o que acontece com a execução do programa.
+
+# objeto = ("cadeira", "mesa", "vaso")
+# objeto.remove("cadeira")
+# objeto.append("caneta")
+# del objeto[0]
+
+# for item in objeto:
+#     print(item)
+
+# --------------------------
+# QUESTÃO 04
+# Considerando as funções a seguir e suas chamadas, indique o que será impresso na tela.
+
+# def funcao(a, b):
+#         a = a + 1
+#         b[1] = a
+    
+# def imprimir(lista):
+#         for item in lista:
+#                 #o end = seguido de duas aspas simples serve para não pular de linha
+#                 print(item, ' ', end= '')
+#         print()
+
+# var = 10
+# lista = [1,2,3]
+# funcao(var, lista)
+# print(var)
+# imprimir(lista)
+
+# ---------------------------------
+
+# QUESTÃO 05
+# Considere o programa Python a seguir. 
+# Marque a alternativa que contém os itens da lista depois de executar o programa.
+
+# lista = [1,2,58,10,19,30,58,100]
+# lista.remove(58)
+# del lista[1]
+# lista.append(-3)
+# lista.insert(2, 77)
+# lista.insert(2, 77)
+
+# print(lista)
