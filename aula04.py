1215621c40c2c4b3e8c1b9cf157a9f893b7bdb31
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-14 19:26:31 -0300

    py

--- aula04.py
+++ aula04.py
@@ -3,12 +3,39 @@
 # Um dicionário é uma coleção não-ordenada de “valores” indexados por “chaves” {}.
 # Dicionários se parecem com listas, mas são mais versáteis
 
-dic = {'chave1':'valor1','chave2':'valor2'}
-print(dic['chave2'])
+# dic = {'chave1':'valor1','chave2':'valor2'}
+# print(dic['chave2'])
 
 # ------------------------
 
-#Criaando uma nova chave:
-dic['chave3'] = 'valor3'
-print(dic)
+#Criando uma nova chave:
+    # Pode-se também atribuir um novo valor, caso já exista esta chave no dicionário
+# dic['chave3'] = 'valor3'
+# print(dic)
+
+# ------------------------
+
+# Removendo um elemento
+# dic.pop('chave1')
+# print(dic)
+
+# ----------------------
+
+# Removendo o último elemento
+# dic.popitem()
+# print(dic)
+
+# --------------------
+
+# Removendo elemento especifico
+# del dic['chave2']
+# print(dic)
+
+# ----------------------
+
+# Removendo o dicionário completo
+# del dic
+# print(dic)
+
+# --------------------------------------------------------------
 
