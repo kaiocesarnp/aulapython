b019664f1441dcce2b52f335a399c1a766691d88
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-14 19:13:41 -0300

    py

---
+++ aula04.py
@@ -0,0 +1,14 @@
+# DICIONÁRIOS
+
+# Um dicionário é uma coleção não-ordenada de “valores” indexados por “chaves” {}.
+# Dicionários se parecem com listas, mas são mais versáteis
+
+dic = {'chave1':'valor1','chave2':'valor2'}
+print(dic['chave2'])
+
+# ------------------------
+
+#Criaando uma nova chave:
+dic['chave3'] = 'valor3'
+print(dic)
+
