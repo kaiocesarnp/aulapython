2ae80d21b83948efcc3b1abb3397a504f3351b39
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-17 17:52:04 -0300

    py

--- aula05.py
+++ aula05.py
@@ -1,1 +1,5 @@
 # STRINGS
+# Assim como para listas convencionais, a função len() vai retornar o tamanho da string (o número de caracteres)
+
+frase = "Curso de Python"
+print("Num caracs:", len(frase))
