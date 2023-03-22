7561b7ef9f07ecca58d3b35c73453a2ed64ffc77
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-22 16:47:33 -0300

    Py

--- aula06.py
+++ aula06.py
@@ -9,5 +9,22 @@
 
 # Depois de instalar o NumPy, basta importar
 # Dica: instale via pip, veja o tutorial
+# import numpy as np
 
-import numpy as np
+# Função Array
+# Os arrays NumPy são chamados de ndarrays
+
+# import numpy as np
+    # #criando a partir de uma lista
+# lista = [1,2,3]
+# array1 = np.array(lista)
+# print("Elemento em zero: ", array1[0])
+# for item in array1:
+#     print(item)
+
+    # #criando diretamente
+# array2 = np.array([4,5,6])
+# for item in array2:
+#     print(item)
+
+# Matrizes
