db5cfa783152c29c469fc7873fdc6fd86da28ad5
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-22 16:35:20 -0300

    numpy

---
+++ aula06.py
@@ -0,0 +1,9 @@
+# NumPy = Numerical Python
+# Biblioteca para Python para lidar com vetores e matrizes “grandes” e de múltiplas dimensões
+# Mitiga muitas das ineficiências computacionais do Python quando lidando com esse tipo de dado
+# Algumas vantagens:
+# Estrutura de dados eficiente para armazenar e operar em arrays (vetores/matrizes)
+# Funções matemáticas prontas para operar em arrays
+# Funções para carga e descarga de dados de/para o disco
+# Funções de álgebra, geração de números aleatórios, transformadas
+
