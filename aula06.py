05cc2f223701e44456b78a344de755a421488d98
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-22 17:05:39 -0300

    py

--- aula06.py
+++ aula06.py
@@ -11,6 +11,7 @@
 # Dica: instale via pip, veja o tutorial
 # import numpy as np
 
+# ---------------
 # Função Array
 # Os arrays NumPy são chamados de ndarrays
 
@@ -27,4 +28,63 @@
 # for item in array2:
 #     print(item)
 
+# ---------------------
 # Matrizes
+# É possível iterar nos itens da matriz, ou acessar diretamente via o operador []
+
+# import numpy as np
+# #criando diretamente
+# array = np.array([[4,5,6],[7,8,9]])
+# for linha in array:
+#     for coluna in linha:
+#         print(coluna, end=' ') #end = ' ' é para nao pular de linha e sim adicionar espaço entre os itens
+#     print() 
+
+# print("Item na linha 1, coluna 2: ", array[1][2])
+
+# ----------------
+# Propriedades
+# Arrays NumPy possuem algumas propriedades que podem seracessadas
+# Exemplos:
+# dtype: qual o tipo dos dados do array
+# shape: qual o formato (dimensões) do array
+# size: tamanho total do array
+# ndim: número de dimensões
+
+# import numpy as np
+
+# vetor = np.array([1.7,2,3,4])
+# matriz = np.array([[4,5,6],[7,8,9]])
+
+# print(vetor.ndim, vetor.shape, vetor.dtype, vetor.size)
+# print(matriz.ndim, matriz.shape, matriz.dtype, matriz.size)
+
+# ------------------
+# Inicialização de Arrays
+
+# Vazios e uns
+# A função zeros(shape, dtype=float) preenche um vetorcom zeros
+# O vetor tem dimensões shape
+# A função ones(shape, dtype=float) faz o mesmo, maspreenche o vetor com uns '1'
+# Por padrão os valores serão floats. O tipo dos valores pode ser modificado pelo parâmetro dtype
+# Veja uma listagem de tipos válidos aqui
+# numpy.org/devdocs/user/basics.types.html
+
+# import numpy as np
+# vetor_zeros = np.zeros(4) #por padrão os elementos dentro do array são float, pos isso o ponto separa os zeros no resultado
+# matriz_zeros = np.zeros((3,2))
+# matriz_uns = np.ones((5,5), dtype=np.intc) # 'intc' é tipo dos dados: inteiros. isso faz com que não seja floats
+
+# print(vetor_zeros)
+# print(matriz_zeros)
+# print(matriz_uns)
+
+# ----------------
+# Full
+# full(shape, fill_value, dtype=None) cria um vetor preenchido com fill_value
+# é possivel especificar o valor que será colocado dentro da matriz
+    #neste exemplo, o valor é o numero 6, transformado em inteiro através de 'intc'
+
+import numpy as np
+matriz_seis = np.full((5,5), 6, dtype=np.intc)
+print(matriz_seis)
