2e769b0885a99dcfc89d5461e344c592f0ab4260
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-24 16:10:09 -0300

    py

--- aula07.py
+++ aula07.py
@@ -446,6 +446,43 @@
 
 # ---------------------
 # Questão 02
-# 
+# Considere o trecho a seguir, e responda como será o gráfico exibido.
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.arange(0,6,1)
+# y1 = x**2
+# y2 = (x**2)/2
+
+# fig = plt.figure()
+# fig.add_subplot(2,1,1)
+# plt.ylabel("f(x)")
+# plt.xlabel("x")
+# fig.add_subplot(2,1,2)
+# plt.plot(x,y1, "k:", label = "y1")
+# plt.plot(x,y2, "b-", label = "y2")
+# plt.legend(loc="upper left")
+# plt.ylabel("f(x)")
+# plt.xlabel("x")
+# plt.show()
+
+# ----------------------
+# Questão 04
+# Ao executar o programa a seguir, o que será armazenado em resultado?
+
+# import numpy as np
+
+# valores = np.random.randint(0,6, (5,5))
+# resultado = np.where((valores > 5) | (valores == 3), valores, 0)
+
+# print(valores)
+# print(resultado)
+
+# Escolha uma opção:
+# a. Um ndarray de 5x5, onde os elementos são 0, 6 ou 3.
+# b. Um ndarray de 5x5, onde os elementos são  0, 1, 2, 4, ou 5.
+# c. Um ndarray de 5x5, onde os elementos são 0 ou 3.                   RESPOSTA
+# d. Um vetor de 25 posições, onde os elementos são  0, 1, 2, 4, ou 5.
+# e. Um ndarray de 5x5, onde os elementos são  0,1,2,4,5 ou 6.
 
