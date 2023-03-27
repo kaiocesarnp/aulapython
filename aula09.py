04d4b92754db3494894cd353890b7168aa209bcc
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-27 17:39:37 -0300

    py

--- aula09.py
+++ aula09.py
@@ -45,8 +45,52 @@
 #Rótulos de coluna (colunas)
             # Isto causa o descarte de índices e valores específicos de um dicionário de Series!!!
 
+# import pandas as pd
 
+# dados = {                               #3 chaves: nome, idade, altura. cada uma com seus valores
+# 'nome': ["Ana", "Bob", "Cleo"], 
+# 'idade': [50, 36, 2],
+# 'altura': [1.5, 1.73, .61]
+# }                                       
 
+# print(pd.Series(dados)) 
+
+# ---------------
+
+# Como criar um DataFrame?
+
+# Forma errada:   
+        # Quando se imprime o dataframe criado a partir de uma série, os indices já foram obtidos
+        # a partir das chaves do dicionário, e como os valores são litas, assume-se que via
+        # 'serie1', há apenas uma coluna (a coluna zero [0]), composta de 3 listas
+import pandas as pd
+
+dados = {
+'nome': ["Ana", "Bob",
+"Cleo"],
+'idade': [50, 36, 2],
+'altura': [1.5, 1.73, .61]
+}
+
+serie1 = pd.Series(dados)
+df1 = pd.DataFrame(serie1)
+print(df1)
+
+# --------------
+# Forma certa:
+                # 3 colunas, sendo as chaves do dicionario. Uma coluna só com nomes, outra só com
+                # idades, outra só com altura.
+import pandas as pd
+
+dados = {
+'nome': ["Ana", "Bob",
+"Cleo"],
+'idade': [50, 36, 2],
+'altura': [1.5, 1.73, .61]
+}
+
+df1 = pd.DataFrame( dados)
+print(df1)
 
 
 
