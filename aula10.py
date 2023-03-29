25daefb599c81f530278497b1312d39671b04c41
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-29 08:53:44 -0300

    py

---
+++ aula10.py
@@ -0,0 +1,8 @@
+# VISUALIZAÇÃO DE DADOS
+
+# Visualização com matplotlib
+# Referências para estudo:
+# ▸ https://matplotlib.org/stable/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py
+# ▸ https://pandas.pydata.org/pandas-docs/stable/user_guide/visualization.html
+
+
