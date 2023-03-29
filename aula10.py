72caf49b4ba240d2079b16353b32d7f8397e7c99
kaiocesarnp <kaiocesarnp@gmail.com>
2023-03-29 08:55:01 -0300

    py

--- aula10.py
+++ aula10.py
@@ -5,4 +5,11 @@
 # ▸ https://matplotlib.org/stable/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py
 # ▸ https://pandas.pydata.org/pandas-docs/stable/user_guide/visualization.html
 
+# A biblioteca “tradicional” para confeccionar gráficos é a MatPlotLib.
+# Pandas faz uso da MatPlotLib de maneira fácil.
+# Para usar os plots do MatPlotLib, além de importar o Pandas no seu programa, faça:
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+
 
